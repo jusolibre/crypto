364d4f239e74f03c6c4596c212b4427b3728fd7f
jusolibre <julien.s@codeur.online>
2017-03-04 12:46:17 +0100

    update push des dossier vide

---
+++ controller/register_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+Class register {
+
+    function register() {
+        ;
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+if (!isset($_SESSION['logged'])) {
+    $_SESSION['logged'] = 0;
+}
+
 if (isset($_GET['p']))
     $params = explode('/', $_GET['p']);
 
